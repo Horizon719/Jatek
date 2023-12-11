0c946be6e5d0a46a4bde81a5221ac6da3bf28d83
Horizon719 <milichovszky.sandor@gmail.com>
2023-12-11 15:20:57 +0100

    targyakat lehet használni

--- src/jatekprogram/Jatek.java
+++ src/jatekprogram/Jatek.java
@@ -4,6 +4,24 @@ public class Jatek {
 
     public static void main(String[] args) {
         Karakter karakter = new Karakter();
+        
+        karakter.felvesz(new Targy("a", "ASD"), 1);
+        karakter.felvesz(new Targy("d", "DAS"), 6);
+        karakter.felvesz(new Targy("a", "ASD"), 1);
+        karakter.felvesz(new Targy("a", "ASD"), 4);
+        karakter.felvesz(new Targy("b", "SAD"));
+        karakter.felvesz(new Targy("c", "DSA"), 3);
+        karakter.felvesz(new Targy("b", "SAD"), 2);
+        karakter.felvesz(new Targy("d", "DAS"), 10);
+        karakter.felvesz(new Targy("d", "DAS"), 2);
+        karakter.felvesz(new Targy("d", "DAS"), 2);
+        karakter.felvesz(new Targy("c", "DSA"), 1);
+        karakter.felvesz(new Targy("b", "SAD"), 1);
+        
+        karakter.hasznal("a", 6);
+        karakter.hasznal("b", 2);
+        karakter.hasznal("c", 5, false);
+        karakter.hasznal("d", 5);
     }
     
 }
--- src/jatekprogram/Karakter.java
+++ src/jatekprogram/Karakter.java
@@ -37,6 +37,37 @@ public class Karakter {
         }
     }
     
+    public void hasznal(String nev){
+        this.hasznal(nev, 1);
+    }
+    
+    public void hasznal(String nev, int mennyit){
+        this.hasznal(nev, mennyit, true);
+    }
+    
+    public void hasznal(String nev, int mennyit, boolean levon){
+        int dbVan = 0;
+        for (Targy elem : felszereles) {
+            if (elem.getNev().equals(nev)) {
+                dbVan++;
+            }
+        }
+        if (levon || dbVan >= mennyit) {
+            int i = 0;
+            int levonandoDb = mennyit;
+            while(i < felszereles.size() && levonandoDb != 0){
+                if(felszereles.get(i).getNev().equals(nev)){
+                    felszereles.remove(i);
+                    levonandoDb--;
+                }
+                i++;
+            }
+            System.out.printf("%s-ból/ből sikeresen felhasználva %d db\n", nev, mennyit);
+        } else {
+            System.out.printf("A %s-t nem sikreült felhasználni! (%d/%d)\n", nev, mennyit, dbVan);
+        }
+    }
+    
     public int dobas(){
         return RND.nextInt(6-1+1)+1;
     }
