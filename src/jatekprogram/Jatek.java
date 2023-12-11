3549adb2ffcdc5b834c638741bff54e7e4979e70
Horizon719 <milichovszky.sandor@gmail.com>
2023-12-11 13:19:25 +0100

    lokális repo

---
+++ src/jatekprogram/Jatek.java
@@ -0,0 +1,9 @@
+package jatekprogram;
+
+public class Jatek {
+
+    public static void main(String[] args) {
+        Karakter karakter = new Karakter();
+    }
+    
+}
---
+++ src/jatekprogram/Karakter.java
@@ -0,0 +1,5 @@
+package jatekprogram;
+
+public class Karakter {
+    
+}
